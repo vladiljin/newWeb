b8853066bc2af820b8782755841a58e859894c6c
vladimir iljin <157532871+vladiljin@users.noreply.github.com>
2024-01-28 11:19:36 +0000

    main.py

--- main.py
+++ main.py
@@ -1,39 +1,33 @@
-from kivy.app import App
-
-from kivy.uix.widget import Widget
-from kivy.clock import Clock
-from jnius import autoclass
-from android.runnable import run_on_ui_thread
-
-
-WebView = autoclass('android.webkit.WebView')
-WebViewClient = autoclass('android.webkit.WebViewClient')
-activity = autoclass('org.kivy.android.PythonActivity').mActivity
-
-
-class Wv(Widget):
-    def __init__(self, **kwargs):
-        super(Wv, self).__init__(**kwargs)
-        Clock.schedule_once(self.create_webview, 0)
-
-    @run_on_ui_thread
-    def create_webview(self, *args):
-        webview = WebView(activity)
-        settings = webview.getSettings()
-        settings.setJavaScriptEnabled(True)
-        settings.setUseWideViewPort(True) # enables viewport html meta tags
-        settings.setLoadWithOverviewMode(True) # uses viewport
-        settings.setSupportZoom(True) # enables zoom
-        settings.setBuiltInZoomControls(True) # enables zoom controls
-        wvc = WebViewClient()
-        webview.setWebViewClient(wvc)
-        activity.setContentView(webview)
-        webview.loadUrl('http://google.com')
-
-class ServiceApp(App):
-    def build(self):
-        return Wv()
-
-
-if __name__ == '__main__':
+import kivy                                                                                     
+from kivy.app import App                                                                        
+from kivy.lang import Builder                                                                   
+from kivy.utils import platform                                                                 
+from kivy.uix.widget import Widget                                                              
+from kivy.clock import Clock                                                                    
+from jnius import autoclass                                                                     
+from android.runnable import run_on_ui_thread                                                   
+                                                                                                
+WebView = autoclass('android.webkit.WebView')                                                   
+WebViewClient = autoclass('android.webkit.WebViewClient')                                       
+activity = autoclass('org.kivy.android.PythonActivity').mActivity                              
+                                                                                                
+class Wv(Widget):                                                                               
+    def __init__(self, **kwargs):                                                               
+        super(Wv, self).__init__(**kwargs)                                                      
+        Clock.schedule_once(self.create_webview, 0)                                             
+                                                                                                
+    @run_on_ui_thread                                                                           
+    def create_webview(self, *args):                                                            
+        webview = WebView(activity)                                                             
+        webview.getSettings().setJavaScriptEnabled(True)                                        
+        wvc = WebViewClient();                                                                  
+        webview.setWebViewClient(wvc);                                                          
+        activity.setContentView(webview)                                                        
+        webview.loadUrl('http://www.google.com')
+                                                                                                
+class ServiceApp(App):                                                                          
+    def build(self):                                                                            
+        return Wv()                                                                             
+                                                                                                
+if __name__ == '__main__':                                                                      
     ServiceApp().run()
